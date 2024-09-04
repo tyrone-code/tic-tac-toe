2eb29c9e4dac74b909cf67a484addfa184e36ace
Tyrone <tschoeman7@gmail.com>
2024-09-04 10:09:15 +0200

    implemented draft logic

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-console.log("hello world!")
+
+
+let gameBoard = {
+    row1:[0,1,2],
+    row2:[3,4,5],
+    row3:[6,7,8]
+}
+
+let gameBoardPositions = {
+    upperLeft:gameBoard.row1[0],
+    upperMiddle:gameBoard.row1[1],
+    upperRight:gameBoard.row1[2],
+    MiddleLeft:gameBoard.row2[0],
+    MiddleMiddle:gameBoard.row2[1],
+    MiddleRight:gameBoard.row2[2],
+
+}
+// var index = items.indexOf(3452);
+
+// if (index !== -1) {
+//     items[index] = 1010;
+// }
+
+
+
+function Player(name,symbol){
+    
+    return {
+        name:name,
+        symbol:symbol,
+        putSymbol(position){
+            if(position === gameBoardPositions.upperLeft){
+                gameBoard.row1[0] = symbol;
+            }else if (position === gameBoardPositions.upperMiddle){
+                gameBoard.row1[1] = symbol;
+            }else if (position === gameBoardPositions.upperRight){
+                gameBoard.row1[2] = symbol;
+
+            }else if (position === gameBoardPositions.MiddleLeft){
+                gameBoard.row2[0] = symbol;
+
+            }else if (position === gameBoardPositions.MiddleMiddle){
+                gameBoard.row2[1] = symbol;
+            }else if (position === gameBoardPositions.MiddleRight){
+                gameBoard.row2[2] = symbol;
+            }
+     
+          
+           return gameBoard;
+        }
+        
+    }
+}
+
+
+player1 = Player("tyrone","x");
+player2 = Player("Felix","O");
+
+player1.putSymbol(gameBoardPositions.upperLeft);
+player1.putSymbol(gameBoardPositions.upperMiddle)
+player1.putSymbol(gameBoardPositions.upperRight)
+
+player1.putSymbol(gameBoardPositions.MiddleMiddle);
+
+
+console.log(gameBoard);
+
+
