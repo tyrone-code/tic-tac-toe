e4bc19f08f9e4a8c0e46473e79edc4ac9f35eda4
Tyrone <tschoeman7@gmail.com>
2024-09-05 07:26:00 +0200

    added gameboard to the webpage

--- app.js
+++ app.js
@@ -6,63 +6,63 @@ let gameBoard = {
     row3:[6,7,8]
 }
 
-let gameBoardPositions = {
-    upperLeft:gameBoard.row1[0],
-    upperMiddle:gameBoard.row1[1],
-    upperRight:gameBoard.row1[2],
-    MiddleLeft:gameBoard.row2[0],
-    MiddleMiddle:gameBoard.row2[1],
-    MiddleRight:gameBoard.row2[2],
+// let gameBoardPositions = {
+//     upperLeft:gameBoard.row1[0],
+//     upperMiddle:gameBoard.row1[1],
+//     upperRight:gameBoard.row1[2],
+//     MiddleLeft:gameBoard.row2[0],
+//     MiddleMiddle:gameBoard.row2[1],
+//     MiddleRight:gameBoard.row2[2],
 
-}
-// var index = items.indexOf(3452);
+// }
+// // var index = items.indexOf(3452);
 
-// if (index !== -1) {
-//     items[index] = 1010;
+// // if (index !== -1) {
+// //     items[index] = 1010;
+// // }
+
+
+
+// function Player(name,symbol){
+    
+//     return {
+//         name:name,
+//         symbol:symbol,
+//         putSymbol(position){
+//             if(position === gameBoardPositions.upperLeft){
+//                 gameBoard.row1[0] = symbol;
+//             }else if (position === gameBoardPositions.upperMiddle){
+//                 gameBoard.row1[1] = symbol;
+//             }else if (position === gameBoardPositions.upperRight){
+//                 gameBoard.row1[2] = symbol;
+
+//             }else if (position === gameBoardPositions.MiddleLeft){
+//                 gameBoard.row2[0] = symbol;
+
+//             }else if (position === gameBoardPositions.MiddleMiddle){
+//                 gameBoard.row2[1] = symbol;
+//             }else if (position === gameBoardPositions.MiddleRight){
+//                 gameBoard.row2[2] = symbol;
+//             }
+     
+          
+//            return gameBoard;
+//         }
+        
+//     }
 // }
 
 
+// player1 = Player("tyrone","x");
+// player2 = Player("Felix","O");
 
-function Player(name,symbol){
-    
-    return {
-        name:name,
-        symbol:symbol,
-        putSymbol(position){
-            if(position === gameBoardPositions.upperLeft){
-                gameBoard.row1[0] = symbol;
-            }else if (position === gameBoardPositions.upperMiddle){
-                gameBoard.row1[1] = symbol;
-            }else if (position === gameBoardPositions.upperRight){
-                gameBoard.row1[2] = symbol;
+// player1.putSymbol(gameBoardPositions.upperLeft);
+// player1.putSymbol(gameBoardPositions.upperMiddle)
+// player1.putSymbol(gameBoardPositions.upperRight)
 
-            }else if (position === gameBoardPositions.MiddleLeft){
-                gameBoard.row2[0] = symbol;
-
-            }else if (position === gameBoardPositions.MiddleMiddle){
-                gameBoard.row2[1] = symbol;
-            }else if (position === gameBoardPositions.MiddleRight){
-                gameBoard.row2[2] = symbol;
-            }
-     
-          
-           return gameBoard;
-        }
-        
-    }
-}
+// player1.putSymbol(gameBoardPositions.MiddleMiddle);
 
 
-player1 = Player("tyrone","x");
-player2 = Player("Felix","O");
-
-player1.putSymbol(gameBoardPositions.upperLeft);
-player1.putSymbol(gameBoardPositions.upperMiddle)
-player1.putSymbol(gameBoardPositions.upperRight)
-
-player1.putSymbol(gameBoardPositions.MiddleMiddle);
-
-
-console.log(gameBoard);
+// console.log(gameBoard);
 
 
