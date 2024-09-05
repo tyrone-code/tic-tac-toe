8d55c5de9a4348bfa005d0c6826a80b3e3d670e2
Tyrone <tschoeman7@gmail.com>
2024-09-05 12:42:39 +0200

    second draft logic

--- app.js
+++ app.js
@@ -1,68 +1,26 @@
 
 
 let gameBoard = {
-    row1:[0,1,2],
-    row2:[3,4,5],
-    row3:[6,7,8]
+    row1 : [0,1,2],
+    row2 : [3,4,5],
+    row3 : [6,7,8]
 }
 
-// let gameBoardPositions = {
-//     upperLeft:gameBoard.row1[0],
-//     upperMiddle:gameBoard.row1[1],
-//     upperRight:gameBoard.row1[2],
-//     MiddleLeft:gameBoard.row2[0],
-//     MiddleMiddle:gameBoard.row2[1],
-//     MiddleRight:gameBoard.row2[2],
-
-// }
-// // var index = items.indexOf(3452);
-
-// // if (index !== -1) {
-// //     items[index] = 1010;
-// // }
 
 
+function displayBlock(){
+    let oneToEight = prompt("enter number from 0 to 8");
+   if (oneToEight === "1"){
+    gameBoard.row1[0] = "x"
+   }else {
+    console.log("1 wasn't pressed!")
+   }
+   console.log(gameBoard)
+}
 
-// function Player(name,symbol){
-    
-//     return {
-//         name:name,
-//         symbol:symbol,
-//         putSymbol(position){
-//             if(position === gameBoardPositions.upperLeft){
-//                 gameBoard.row1[0] = symbol;
-//             }else if (position === gameBoardPositions.upperMiddle){
-//                 gameBoard.row1[1] = symbol;
-//             }else if (position === gameBoardPositions.upperRight){
-//                 gameBoard.row1[2] = symbol;
-
-//             }else if (position === gameBoardPositions.MiddleLeft){
-//                 gameBoard.row2[0] = symbol;
-
-//             }else if (position === gameBoardPositions.MiddleMiddle){
-//                 gameBoard.row2[1] = symbol;
-//             }else if (position === gameBoardPositions.MiddleRight){
-//                 gameBoard.row2[2] = symbol;
-//             }
-     
-          
-//            return gameBoard;
-//         }
+ function createPlayer(name,symbol){
         
-//     }
-// }
+}
 
 
-// player1 = Player("tyrone","x");
-// player2 = Player("Felix","O");
-
-// player1.putSymbol(gameBoardPositions.upperLeft);
-// player1.putSymbol(gameBoardPositions.upperMiddle)
-// player1.putSymbol(gameBoardPositions.upperRight)
-
-// player1.putSymbol(gameBoardPositions.MiddleMiddle);
-
-
-// console.log(gameBoard);
-
-
+displayBlock();
