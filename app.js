e543652596e16c6ede761f5ae80243db8c24357a
Tyrone <tschoeman7@gmail.com>
2024-09-11 15:04:19 +0200

    added all 9 boxes that can be pressed and shown on the dom

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 let topLeft = document.getElementById("b-1");
 let topMiddle = document.getElementById("b-2");
 let topRight = document.getElementById("b-3");
-let middleRight = document.getElementById("b-4");
+let middleLeft = document.getElementById("b-4");
 let middleMiddle = document.getElementById("b-5");
+let middleRight = document.getElementById("b-6");
+let bottomLeft = document.getElementById("b-7");
+let bottomMiddle = document.getElementById("b-8");
+let bottomRight = document.getElementById("b-9");
+
+let h1 = document.getElementById("decideWinner");
 
 
 
@@ -120,16 +126,204 @@ topMiddle.addEventListener("click", function () {
 
  } });
 
+ middleLeft.addEventListener("click", function () {
+  if(player1.playerTurn && gameBoard.row2[0] !== player2.symbol && gameBoard.row2[0] !== player1.symbol){
+    displaytoDom(player1,middleLeft);
+    gameBoard.row2[0] = player1.symbol;
+    player1.playerTurn = false;
+    player2.playerTurn = true;
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+ }else if (player2.playerTurn && gameBoard.row2[0] !== player2.symbol && gameBoard.row2[0] !== player1.symbol){
+   displaytoDom(player2,middleLeft);  
+   gameBoard.row2[0] = player2.symbol;
+   player2.playerTurn = false;
+    player1.playerTurn = true;     
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+} });
+
+
+
+middleMiddle.addEventListener("click", function () {
+  if(player1.playerTurn && gameBoard.row2[1] !== player2.symbol && gameBoard.row2[1] !== player1.symbol){
+    displaytoDom(player1,middleMiddle);
+    gameBoard.row2[1] = player1.symbol;
+    player1.playerTurn = false;
+    player2.playerTurn = true;
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+ }else if (player2.playerTurn && gameBoard.row2[1] !== player2.symbol && gameBoard.row2[1] !== player1.symbol){
+   displaytoDom(player2,middleMiddle);  
+   gameBoard.row2[1] = player2.symbol;
+   player2.playerTurn = false;
+    player1.playerTurn = true;     
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+} });
+
+
+middleRight.addEventListener("click", function () {
+  if(player1.playerTurn && gameBoard.row2[2] !== player2.symbol && gameBoard.row2[2] !== player1.symbol){
+    displaytoDom(player1,middleRight);
+    gameBoard.row2[2] = player1.symbol;
+    player1.playerTurn = false;
+    player2.playerTurn = true;
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+ }else if (player2.playerTurn && gameBoard.row2[2] !== player2.symbol && gameBoard.row2[2] !== player1.symbol){
+   displaytoDom(player2,middleRight);  
+   gameBoard.row2[2] = player2.symbol;
+   player2.playerTurn = false;
+    player1.playerTurn = true;     
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+} });
+
+
+
+
+
+
+bottomLeft.addEventListener("click", function () {
+  if(player1.playerTurn && gameBoard.row3[0] !== player2.symbol && gameBoard.row3[0] !== player1.symbol){
+    displaytoDom(player1,bottomLeft);
+    gameBoard.row3[0] = player1.symbol;
+    player1.playerTurn = false;
+    player2.playerTurn = true;
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+ }else if (player2.playerTurn && gameBoard.row3[0] !== player2.symbol && gameBoard.row3[0] !== player1.symbol){
+   displaytoDom(player2,bottomLeft);  
+   gameBoard.row3[0] = player2.symbol;
+   player2.playerTurn = false;
+    player1.playerTurn = true;     
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+} });
+
+
+
+
+bottomMiddle.addEventListener("click", function () {
+  if(player1.playerTurn && gameBoard.row3[1] !== player2.symbol && gameBoard.row3[1] !== player1.symbol){
+    displaytoDom(player1,bottomMiddle);
+    gameBoard.row3[1] = player1.symbol;
+    player1.playerTurn = false;
+    player2.playerTurn = true;
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+ }else if (player2.playerTurn && gameBoard.row3[1] !== player2.symbol && gameBoard.row3[1] !== player1.symbol){
+   displaytoDom(player2,bottomMiddle);  
+   gameBoard.row3[1] = player2.symbol;
+   player2.playerTurn = false;
+    player1.playerTurn = true;     
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+} });
+
+
+
+
+
+bottomRight.addEventListener("click", function () {
+  if(player1.playerTurn && gameBoard.row3[2] !== player2.symbol && gameBoard.row3[2] !== player1.symbol){
+    displaytoDom(player1,bottomRight);
+    gameBoard.row3[2] = player1.symbol;
+    player1.playerTurn = false;
+    player2.playerTurn = true;
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+ }else if (player2.playerTurn && gameBoard.row3[2] !== player2.symbol && gameBoard.row3[2] !== player1.symbol){
+   displaytoDom(player2,bottomRight);  
+   gameBoard.row3[2] = player2.symbol;
+   player2.playerTurn = false;
+    player1.playerTurn = true;     
+    console.log(player1)
+    console.log(player2)
+    console.log(gameBoard)
+    decideWinner();
+
+
+} });
+
+
+
+
+
+
+
+
+
+
 
 let decideWinner = function(){
   let topRow = gameBoard.row1.join("");
-   if (topRow === "XOX"){
+  let middleRow = gameBoard.row2.join("");
 
-   }
+   if (topRow === "XXX"){
+      topLeft.style.backgroundColor = "LightGreen"
+      topMiddle.style.backgroundColor = "LightGreen"
+      topRight.style.backgroundColor = "LightGreen"
+
+      h1.innerHTML = " X is the WINNER!"
+  
+   }else if (middleRow === "XXX") {
+    middleLeft.style.backgroundColor = "LightGreen"
+    middleMiddle.style.backgroundColor = "LightGreen"
+    middleRight.style.backgroundColor = "LightGreen"
+    
+
+  }else if (gameBoard.row1 === gameBoard.row1) {
+    h1.innerHTML = "draw!";
+}
+
  
   }
-
-  decideWinner();
 
   // let diagonal1 = gameBoard.row1[0]
   // let diagonal2 = gameBoard.row2[1]
