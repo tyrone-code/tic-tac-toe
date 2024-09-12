a5cb0a5a280c235fb6d797b27079bfd471bd7fb8
Tyrone <tschoeman7@gmail.com>
2024-09-12 08:13:41 +0200

    when a player wins added a function that resets values

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ function playerSymbol(symbol,playerTurn){
 
 let player1 = playerSymbol("X",true);
 let player2 = playerSymbol("O",false);
+h1.innerHTML = "X's TURN"
 
 
 let displaytoDom = function(player,position){
@@ -50,9 +51,7 @@ topLeft.addEventListener("click", function () {
              gameBoard.row1[0] = player1.symbol;
              player1.playerTurn = false;
              player2.playerTurn = true;
-             console.log(player1)
-             console.log(player2)
-             console.log(gameBoard)
+            h1.innerHTML = "O's TURN"
              decideWinner();
 
 
@@ -61,9 +60,8 @@ topLeft.addEventListener("click", function () {
             gameBoard.row1[0] = player2.symbol;
             player2.playerTurn = false;
              player1.playerTurn = true;     
-             console.log(player1)
-             console.log(player2)
-             console.log(gameBoard)
+             h1.innerHTML = "X's TURN"
+
              decideWinner();
 
 
@@ -79,9 +77,7 @@ topMiddle.addEventListener("click", function () {
     gameBoard.row1[1] = player1.symbol;
     player1.playerTurn = false;
     player2.playerTurn = true;
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+  h1.innerHTML = "O's TURN"
     decideWinner();
 
 
@@ -90,9 +86,7 @@ topMiddle.addEventListener("click", function () {
    gameBoard.row1[1] = player2.symbol;
    player2.playerTurn = false;
     player1.playerTurn = true;     
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+ h1.innerHTML = "X's TURN"
     decideWinner();
 
 
@@ -107,9 +101,7 @@ topMiddle.addEventListener("click", function () {
       gameBoard.row1[2] = player1.symbol;
       player1.playerTurn = false;
       player2.playerTurn = true;
-      console.log(player1)
-      console.log(player2)
-      console.log(gameBoard)
+      h1.innerHTML = "O's TURN"
       decideWinner();
 
 
@@ -118,24 +110,22 @@ topMiddle.addEventListener("click", function () {
      gameBoard.row1[2] = player2.symbol;
      player2.playerTurn = false;
       player1.playerTurn = true;     
-      console.log(player1)
-      console.log(player2)
-      console.log(gameBoard)
+      h1.innerHTML = "X's TURN"
       decideWinner();
 
 
  } });
 
  middleLeft.addEventListener("click", function () {
+
   if(player1.playerTurn && gameBoard.row2[0] !== player2.symbol && gameBoard.row2[0] !== player1.symbol){
     displaytoDom(player1,middleLeft);
     gameBoard.row2[0] = player1.symbol;
     player1.playerTurn = false;
     player2.playerTurn = true;
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "O's TURN"
     decideWinner();
+ 
 
 
  }else if (player2.playerTurn && gameBoard.row2[0] !== player2.symbol && gameBoard.row2[0] !== player1.symbol){
@@ -143,9 +133,7 @@ topMiddle.addEventListener("click", function () {
    gameBoard.row2[0] = player2.symbol;
    player2.playerTurn = false;
     player1.playerTurn = true;     
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "X's TURN"
     decideWinner();
 
 
@@ -159,9 +147,8 @@ middleMiddle.addEventListener("click", function () {
     gameBoard.row2[1] = player1.symbol;
     player1.playerTurn = false;
     player2.playerTurn = true;
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "O's TURN"
+
     decideWinner();
 
 
@@ -170,9 +157,8 @@ middleMiddle.addEventListener("click", function () {
    gameBoard.row2[1] = player2.symbol;
    player2.playerTurn = false;
     player1.playerTurn = true;     
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "X's TURN"
+
     decideWinner();
 
 
@@ -185,22 +171,18 @@ middleRight.addEventListener("click", function () {
     gameBoard.row2[2] = player1.symbol;
     player1.playerTurn = false;
     player2.playerTurn = true;
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "O's TURN"
     decideWinner();
-
 
  }else if (player2.playerTurn && gameBoard.row2[2] !== player2.symbol && gameBoard.row2[2] !== player1.symbol){
    displaytoDom(player2,middleRight);  
    gameBoard.row2[2] = player2.symbol;
    player2.playerTurn = false;
     player1.playerTurn = true;     
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+
     decideWinner();
 
+    h1.innerHTML = "X's TURN"
 
 } });
 
@@ -215,9 +197,8 @@ bottomLeft.addEventListener("click", function () {
     gameBoard.row3[0] = player1.symbol;
     player1.playerTurn = false;
     player2.playerTurn = true;
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "O's TURN"
+
     decideWinner();
 
 
@@ -226,9 +207,8 @@ bottomLeft.addEventListener("click", function () {
    gameBoard.row3[0] = player2.symbol;
    player2.playerTurn = false;
     player1.playerTurn = true;     
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "X's TURN"
+
     decideWinner();
 
 
@@ -243,9 +223,8 @@ bottomMiddle.addEventListener("click", function () {
     gameBoard.row3[1] = player1.symbol;
     player1.playerTurn = false;
     player2.playerTurn = true;
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "O's TURN"
+
     decideWinner();
 
 
@@ -254,9 +233,8 @@ bottomMiddle.addEventListener("click", function () {
    gameBoard.row3[1] = player2.symbol;
    player2.playerTurn = false;
     player1.playerTurn = true;     
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "X's TURN"
+
     decideWinner();
 
 
@@ -272,9 +250,8 @@ bottomRight.addEventListener("click", function () {
     gameBoard.row3[2] = player1.symbol;
     player1.playerTurn = false;
     player2.playerTurn = true;
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "O's TURN"
+
     decideWinner();
 
 
@@ -283,9 +260,8 @@ bottomRight.addEventListener("click", function () {
    gameBoard.row3[2] = player2.symbol;
    player2.playerTurn = false;
     player1.playerTurn = true;     
-    console.log(player1)
-    console.log(player2)
-    console.log(gameBoard)
+    h1.innerHTML = "X's TURN"
+
     decideWinner();
 
 
@@ -293,39 +269,147 @@ bottomRight.addEventListener("click", function () {
 
 
 
-
-
-
-
-
-
-
-
 let decideWinner = function(){
+  let diagonal1 = gameBoard.row1[0]
+  let diagonal2 = gameBoard.row2[1]
+  let diagonal3 = gameBoard.row3[2]
+  let captureDiagonal = [diagonal1, diagonal2 , diagonal3]
+  let diagonalTopLeftBottomRight =  captureDiagonal.join("")
+  let column1Left = gameBoard.row1[0];
+  let column2Left = gameBoard.row2[0];
+  let column3Left = gameBoard.row3[0];
+  let captureLeftColumn = [column1Left,column2Left,column3Left];
+  let column1Middle = gameBoard.row1[1];
+  let column2Middle = gameBoard.row2[1];
+  let column3Middle = gameBoard.row3[1];
+  let column1Right = gameBoard.row1[2];
+  let column2Right = gameBoard.row2[2];
+  let column3Right = gameBoard.row3[2];
+  let captureMiddleColumn = [column1Middle,column2Middle,column3Middle];
+  let captureRightColumn = [column1Right,column2Right,column3Right];
+  let leftColumn = captureLeftColumn.join("")
+  let rightColumn = captureRightColumn.join("")
+  let middleColumn = captureMiddleColumn.join("")
   let topRow = gameBoard.row1.join("");
   let middleRow = gameBoard.row2.join("");
+  let bottomRow = gameBoard.row3.join("")
 
    if (topRow === "XXX"){
       topLeft.style.backgroundColor = "LightGreen"
       topMiddle.style.backgroundColor = "LightGreen"
       topRight.style.backgroundColor = "LightGreen"
 
-      h1.innerHTML = " X is the WINNER!"
+      h1.innerHTML = "X is the WINNER!"
+      setTimeout(() => {
+        resetValues();
   
+      }, "2500");
+ 
    }else if (middleRow === "XXX") {
     middleLeft.style.backgroundColor = "LightGreen"
     middleMiddle.style.backgroundColor = "LightGreen"
     middleRight.style.backgroundColor = "LightGreen"
-    
+    h1.innerHTML = " X is the WINNER!"
+    setTimeout(() => {
+      resetValues();
 
-  }else if (gameBoard.row1 === gameBoard.row1) {
-    h1.innerHTML = "draw!";
+    }, "2500");
+
+   }
+    else if (bottomRow === "XXX") {
+      bottomLeft.style.backgroundColor = "LightGreen"
+      bottomMiddle.style.backgroundColor = "LightGreen"
+      bottomRight.style.backgroundColor = "LightGreen"
+       h1.innerHTML = " X is the WINNER!"
+       setTimeout(() => {
+        resetValues();
+  
+      }, "2500");
+ 
+    }
+       else if (leftColumn === "XXX") {
+        topLeft.style.backgroundColor = "LightGreen"
+        middleLeft.style.backgroundColor = "LightGreen"
+        bottomLeft.style.backgroundColor = "LightGreen"
+         h1.innerHTML = " X is the WINNER!"
+         setTimeout(() => {
+          resetValues();
+    
+        }, "2500");
+   
+       }
+         else if (middleColumn === "XXX") {
+          topMiddle.style.backgroundColor = "LightGreen"
+          middleMiddle.style.backgroundColor = "LightGreen"
+          bottomMiddle.style.backgroundColor = "LightGreen"
+           h1.innerHTML = "X is the WINNER!"
+           setTimeout(() => {
+            resetValues();
+      
+          }, "2500");
+     
+         }
+           else if (rightColumn === "XXX") {
+            topRight.style.backgroundColor = "LightGreen"
+            middleRight.style.backgroundColor = "LightGreen"
+            bottomRight.style.backgroundColor = "LightGreen"
+             h1.innerHTML = "X is the WINNER!"
+             setTimeout(() => {
+              resetValues();
+        
+            }, "2500");
+       
+}else if (diagonalTopLeftBottomRight === "XXX") {
+  topLeft.style.backgroundColor = "LightGreen"
+  middleMiddle.style.backgroundColor = "LightGreen"
+  bottomRight.style.backgroundColor = "LightGreen"
+  setTimeout(() => {
+    resetValues();
+
+  }, "2500");
+
+  h1.innerHTML = " X is the WINNER!"
 }
 
- 
+
   }
 
-  // let diagonal1 = gameBoard.row1[0]
-  // let diagonal2 = gameBoard.row2[1]
-  // let diagonal3 = gameBoard.row3[2]
-  // let diagonal = [diagonal1, diagonal2 , diagonal3]
+function resetValues(){
+  player1.playerTurn = true; 
+  player2.playerTurn = false;
+  
+  h1.innerHTML = ""
+
+  topLeft.innerHTML = ""
+  topMiddle.innerHTML = ""
+  topRight.innerHTML = ""
+  ////
+  middleLeft.innerHTML = ""
+  middleMiddle.innerHTML = ""
+  middleRight.innerHTML = ""
+  ////
+  bottomLeft.innerHTML = ""
+  bottomMiddle.innerHTML = ""
+  bottomRight.innerHTML = ""
+  ////
+  topLeft.style.backgroundColor = ""
+      topMiddle.style.backgroundColor = ""
+      topRight.style.backgroundColor = ""
+      ////
+ 
+      middleLeft.style.backgroundColor = ""
+      middleMiddle.style.backgroundColor = ""
+      middleRight.style.backgroundColor = ""
+
+      //
+
+      bottomLeft.style.backgroundColor = ""
+      bottomMiddle.style.backgroundColor = ""
+      bottomRight.style.backgroundColor = ""
+      //
+  gameBoard = {
+    row1 : [0,1,2],
+    row2 : [0,1,2],
+    row3 : [0,1,2]
+}
+}
